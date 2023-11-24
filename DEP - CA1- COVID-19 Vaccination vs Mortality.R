c0d1b467b21592f12bc810f8bbcdfb072594ed23
Danrlei Martins <2020322@student.cct.ie>
2023-11-24 15:23:24 +0000

    Dataset Overview and Cleaning
    - Dataset overview
    - Dataset cleaning

--- DEP - CA1- COVID-19 Vaccination vs Mortality.R
+++ DEP - CA1- COVID-19 Vaccination vs Mortality.R
@@ -2,4 +2,30 @@
 setwd("C:/Users/danrl/OneDrive/Documents/R Programming/DEP-CA1-Project/")
 
 # Load dataset
-dataset = read.csv("covid-vaccination-vs-death_ratio.csv")
+covid_dataset = read.csv("covid-vaccination-vs-death_ratio.csv")
+
+# Load necessary libraries
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(viridis)
+
+# 1. Data Overview
+
+# Display first and last rows
+head(covid_dataset)
+tail(covid_dataset)
+
+# Get dimensions
+dim(covid_dataset)
+
+# Display stats summary of dataset
+summary(covid_dataset)
+
+# 2. Data Cleaning
+
+# Renaming column
+colnames(covid_dataset)[8] <- "new_deaths"
+
+# Drop unnecessary column
+covid_dataset <- subset(covid_dataset, select = -X)
