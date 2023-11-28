4904fa9e57d5a78c1513670467948271fda0c178
Danrlei Martins <2020322@student.cct.ie>
2023-11-28 12:25:03 +0000

    Statistical Parameters Calculation
    - Calculated the mean, median, min, max, and standard deviation of all numeric variables.

--- DEP - CA1- COVID-19 Vaccination vs Mortality.R
+++ DEP - CA1- COVID-19 Vaccination vs Mortality.R
@@ -52,7 +52,7 @@ covid_dataset$date <- as.Date(covid_dataset$date)
 cat("\nStructure of the dataset:\n")
 str(covid_dataset)
 
-# Categorical variable - country
+# 3.1 Categorical variable - country
 
 # Select the top 10 countries by total vaccinations (dataset contains 197)
 top_10_countries <- covid_dataset %>%
@@ -84,7 +84,7 @@ ggplot(top_10_countries,
             color = "white",
             position = position_stack(vjust = 0.5))
 
-# Discrete variable - date
+# 3.2 Discrete variable - date
 covid_dataset %>%
   group_by(date) %>%
   summarize(count = n()) %>%
@@ -100,7 +100,7 @@ ggplot(subset(covid_dataset, country %in% c("Germany", "The United Kingdom")),
   labs(title = "Germany vs UK - Total Vaccinations Over Time", x = "Date", y = "Total Vaccinations") +
   theme(legend.position = "right")
 
-# Continuous Variables - people_vaccinated & people_fully_vaccinated
+# 3.3 Continuous Variables - people_vaccinated & people_fully_vaccinated
 ggplot(subset(covid_dataset, country %in% c("The United Kingdom")), aes(x = date)) +
   geom_area(aes(y = percentage_fully_vaccinated, 
                 fill = "Fully Vaccinated"), 
@@ -123,9 +123,69 @@ ggplot(subset(covid_dataset, country %in% c("The United Kingdom")), aes(x = date
   scale_fill_manual(values = c("Fully Vaccinated" = "blue", "Partially Vaccinated" = "orange")) +
   scale_color_manual(values = c("Fully Vaccinated" = "blue", "Partially Vaccinated" = "orange"))
 
+# 3.4 Continuous variable - new_deaths
+ggplot(covid_dataset, aes(x = date, y = new_deaths)) +
+  geom_bar(stat = "identity", fill = "orange") +
+  labs(title = "New Deaths Over Time",
+       x = "Date",
+       y = "Number of New Deaths") +
+  theme_minimal()
+
+# Dealing with negative values
+
+# Check minimum value of new_deaths
+summary(covid_dataset$new_deaths)
+
+negative_deaths <- covid_dataset[covid_dataset$new_deaths < 0, ]
+
+# Assuming the negative values are typos, fixing the values
+covid_dataset$new_deaths <- ifelse(covid_dataset$new_deaths < 0, -covid_dataset$new_deaths, covid_dataset$new_deaths)
+
+# 3.5 Continuous variable - ratio
+# to be finished
+
 # 4. Missing Values Exploration
 
 # Check for missing data
 cat("\nMissing Data:\n")
 print(colSums(is.na(covid_dataset)))
 
+# 5. Statistical Parameters Calculation
+
+mean_total_vaccinations <- mean(covid_dataset$total_vaccinations)
+median_total_vaccinations <- median(covid_dataset$total_vaccinations)
+min_total_vaccinations <- min(covid_dataset$total_vaccinations)
+max_total_vaccinations <- max(covid_dataset$total_vaccinations)
+sd_total_vaccinations <- sd(covid_dataset$total_vaccinations)
+
+mean_people_vaccinated <- mean(covid_dataset$people_vaccinated)
+median_people_vaccinated <- median(covid_dataset$people_vaccinated)
+min_people_vaccinated <- min(covid_dataset$people_vaccinated)
+max_people_vaccinated <- max(covid_dataset$people_vaccinated)
+sd_people_vaccinated <- sd(covid_dataset$people_vaccinated)
+
+mean_people_fully_vaccinated <- mean(covid_dataset$people_fully_vaccinated)
+median_people_fully_vaccinated <- median(covid_dataset$people_fully_vaccinated)
+min_people_fully_vaccinated <- min(covid_dataset$people_fully_vaccinated)
+max_people_fully_vaccinated <- max(covid_dataset$people_fully_vaccinated)
+sd_people_fully_vaccinated <- sd(covid_dataset$people_fully_vaccinated)
+
+mean_new_deaths <- mean(covid_dataset$new_deaths)
+median_new_deaths <- median(covid_dataset$new_deaths)
+min_new_deaths <- min(covid_dataset$new_deaths)
+max_new_deaths <- max(covid_dataset$new_deaths)
+sd_new_deaths <- sd(covid_dataset$new_deaths)
+
+mean_population <- mean(covid_dataset$population)
+median_population <- median(covid_dataset$population)
+min_population <- min(covid_dataset$population)
+max_population <- max(covid_dataset$population)
+sd_population <- sd(covid_dataset$population)
+
+mean_ratio <- mean(covid_dataset$ratio)
+median_ratio <- median(covid_dataset$ratio)
+min_ratio <- min(covid_dataset$ratio)
+max_ratio <- max(covid_dataset$ratio)
+sd_ratio <- sd(covid_dataset$ratio)
+
+# 6. Normalization and Standardization
